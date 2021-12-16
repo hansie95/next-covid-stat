b8e1563a62b0c805d6fff34a97d03bd3dd74d5c7
hansie95 <hansie.schmidt95@gmail.com>
2021-12-16 11:13:43 +0100

    feat(Covid-stat): new API implemented

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-import { Input, Box, Tooltip } from "@chakra-ui/react";
+import { Input, Box, Tooltip, Button } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { testActions } from "../store/slices/slice";
 import { countryIdSelector } from "../store/selectors/selector";
@@ -15,16 +15,23 @@ const SearchBar = () => {
     [dispatch]
   );
 
+  const dataFetchHandler = useCallback(() => {
+    dispatch(testActions.fetch());
+  }, [dispatch]);
+
   console.log(country);
 
   return (
     <Box w="30%" m="auto">
-      <Tooltip label="IT, DE or Italy, Deutschland">
+      <Tooltip label="IT, DE or Italy, Deutschland" placement="top">
         <Input
           placeholder="Country abbreviation or full country name"
           onChange={countryIdChangeHandler}
         />
       </Tooltip>
+      <Button mt={2} size={"lg"} onClick={dataFetchHandler}>
+        Load covid stats!
+      </Button>
     </Box>
   );
 };
--- store/saga.ts
+++ store/saga.ts
@@ -1,42 +1,27 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { call, put, takeLatest, select } from "redux-saga/effects";
-import { createInferTypeNode } from "typescript";
 import { countryIdSelector } from "./selectors/selector";
 import { testActions } from "./slices/slice";
 
-const options: AxiosRequestConfig = {
+var options: AxiosRequestConfig = {
   method: "GET",
-  url: "",
-  params: {},
+  url: "https://covid-193.p.rapidapi.com/countries",
+  params: { search: "" },
   headers: {
-    "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+    "x-rapidapi-host": "covid-193.p.rapidapi.com",
     "x-rapidapi-key": "079576c957msh05a475683c15090p1f4ec2jsnae7fbb84f1a1",
   },
 };
 
-const url: string[] = [
-  "https://covid-19-data.p.rapidapi.com/country",
-  "https://covid-19-data.p.rapidapi.com/country/code",
-];
-
-const paramsChoice = (country: string) => {
-  if (country.length <= 3) {
-    (options.url = url[1]), (options.params = { code: country });
-  } else {
-    (options.url = url[0]), (options.params = { name: country });
-  }
-};
-
 const collectDataFromApi = async (country: string) => {
-  paramsChoice(country);
-  console.log(options);
+  options.params.search = country;
   const { data: data } = await axios.request(options);
   return { data };
 };
 
 function* handleApiCall() {
-  const cityId: string = yield select(countryIdSelector);
   try {
+    const cityId: string = yield select(countryIdSelector);
     const { data } = yield call(collectDataFromApi, cityId);
     yield put(testActions.AddCovidData({ covidData: data }));
   } catch (err) {
@@ -45,6 +30,7 @@ function* handleApiCall() {
 }
 export function* rootSaga() {
   yield takeLatest(testActions.fetch.type, handleApiCall);
+  console.log(options);
 }
 
 export default rootSaga;
