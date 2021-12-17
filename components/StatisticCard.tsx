995bbc5c222a096f2fe4eb93f497078825a216b4
hansie95 <hansie.schmidt95@gmail.com>
2021-12-17 14:52:56 +0100

    feat(Covid-Stat): unused components deleted

--- components/StatisticCard.tsx
+++ components/StatisticCard.tsx
@@ -1,12 +1,5 @@
 import { Box, Center, Heading, Text } from "@chakra-ui/layout";
-import {
-  SimpleGrid,
-  Flex,
-  Image,
-  Grid,
-  GridItem,
-  HStack,
-} from "@chakra-ui/react";
+import { SimpleGrid, Image } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import {
@@ -46,49 +39,6 @@ const StatisticCard = () => {
           <Box height="110px"></Box>
         </SimpleGrid>
       </Box>
-      /* <Box mx="30px" w="50%">
-        <Box>
-          
-        </Box>
-
-        <SimpleGrid spacing="40px">
-          <Box bg="tomato" height="80px">
-            <Image w="70px" borderRadius="50%" src="/grave.png" alt="Grave" />
-          </Box>
-          <Box bg="tomato" height="80px"></Box>
-          <Box bg="tomato" height="80px"></Box>
-        </SimpleGrid>
-      </Box> */
-      /* 
-      <Grid
-        h="200px"
-        w="50%"
-        mx="30px"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(5, 1fr)"
-        gap={4}
-        > */
-
-      /*  <GridItem rowSpan={2} colSpan={1} bg="tomato" /> */
-      /*   <GridItem colSpan={6}>
-          <Heading>{country.response[0].country}</Heading>
-          <Text as="i">Latest update: {country.response[0].day}</Text>
-        </GridItem>
-        <Flex w="300px">
-          <Center m="auto" mr="160px">
-            <Image src="/grave.png" alt="grave" w="90px" />
-            <Heading ml="20px">
-              Deaths {country.response[0].deaths.total}
-            </Heading>
-          </Center>
-          <Center>
-            <Image src="/population.png" alt="population" w="110px" />
-            <Heading ml="20px">
-              Pupulation {country.response[0].population}
-            </Heading>
-          </Center>
-        </Flex>
-      </Grid> */
     );
   }
 
