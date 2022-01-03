ad45029ad2502f4775fbf08a8d773c7ca9483417
hansie95 <hansie.schmidt95@gmail.com>
2022-01-03 14:21:31 +0100

    feat(Covid-stat): Layout modification and logic added

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,11 @@
 import React, { useCallback } from "react";
 import { Input, Box, Tooltip } from "@chakra-ui/react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { testActions } from "../store/slices/slice";
-import {
-  countryIdSelector,
-  testData2Selector,
-} from "../store/selectors/selector";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-  const country = useSelector(countryIdSelector);
-  const data2 = useSelector(testData2Selector);
-
   const countryIdChangeHandler = useCallback(
     (event) => {
       dispatch(testActions.AddCountryId(event.target.value));
@@ -23,10 +16,7 @@ const SearchBar = () => {
   return (
     <Box w="30%" m="auto">
       <Tooltip label="Italy, Slovakia, Hungary etc..." placement="top">
-        <Input
-          placeholder="Country abbreviation or full country name"
-          onChange={countryIdChangeHandler}
-        />
+        <Input placeholder="Country name" onChange={countryIdChangeHandler} />
       </Tooltip>
     </Box>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,18 @@
 import React, { FC, useCallback } from "react";
-import { useSelector } from "react-redux";
-import { Box, Heading, Button, Flex, Grid } from "@chakra-ui/react";
+import { Box, Heading, Button } from "@chakra-ui/react";
 import { useDispatch } from "react-redux";
 
 import { testActions } from "../store/slices/slice";
-import {
-  testData2Selector,
-  testDataSelector,
-} from "../store/selectors/selector";
 import SearchBar from "../components/SearchBar";
 import { Picture } from "../styles/SpinPicture.style";
-import MapChart from "../components/Map";
-import StatisticCard from "../components/StatisticCard";
+import CountryDatas from "../components/CountryDatas";
 
 const Home: FC = () => {
-  const data = useSelector(testDataSelector);
-  const data2 = useSelector(testData2Selector);
   const dispatch = useDispatch();
 
   const dataFetchHandler = useCallback(() => {
     dispatch(testActions.fetch());
   }, [dispatch]);
-
-  console.log(data);
-  console.log(data2);
 
   return (
     <>
@@ -34,31 +23,16 @@ const Home: FC = () => {
           width="100px"
           height="100px"
         />
-        <Heading as="h1" size="2xl">
+        <Heading mt="15px" as="h1" size="2xl">
           Welcome Covid Stat v1
         </Heading>
-        <Box mt="10">
+        <Box my="10">
           <SearchBar />
           <Button mt={2} size={"lg"} onClick={dataFetchHandler}>
             Load covid stats!
           </Button>
         </Box>
-        {data.length !== 0 && data.errors !== "undefined" ? (
-          <Heading m={10} as="h3" size="xl">
-            Data fetched! Check It in the console!
-          </Heading>
-        ) : (
-          <Heading m={10} as="h3" size="xl">
-            There is no data fetched!
-          </Heading>
-        )}
-
-        <Flex>
-          <Box mx="30px" w="50%" boxShadow="dark-lg" rounded="md" bg="white">
-            <MapChart />
-          </Box>
-          <StatisticCard />
-        </Flex>
+        <CountryDatas />
       </Box>
     </>
   );
