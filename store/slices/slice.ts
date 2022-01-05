3a2919ce8cdafa26bda9e4db01956a100d800365
hansie95 <hansie.schmidt95@gmail.com>
2022-01-05 16:28:08 +0100

    feat(Covid-stat): Create new slice to region

--- store/slices/slice.ts
+++ store/slices/slice.ts
@@ -5,6 +5,8 @@ export type TestState = {
   data2: any;
   countryId: string;
   longlat: [number, number];
+  region: string;
+  view: any;
 };
 
 export const initialState: TestState = {
@@ -12,12 +14,35 @@ export const initialState: TestState = {
   data2: [],
   longlat: [0, 0],
   countryId: "",
+  region: "",
+  view: {},
 };
 
 const test = createSlice({
   name: "test",
   initialState,
   reducers: {
+    RegionOptimizer(state, { payload }: PayloadAction<any>) {
+      if (payload.continent === "South-America") {
+        state.region = "Americas";
+        state.view = { rotate: [58, 20, 0], scale: 400 };
+      } else if (payload.continent === "Oceania") {
+        state.region = "Oceania";
+        state.view = { rotate: [215, 25, 10], scale: 710 };
+      } else if (payload.continent === "North-America") {
+        state.region = "Americas";
+        state.view = { rotate: [100, -50, 10], scale: 400 };
+      } else if (payload.continent === "Africa") {
+        state.region = "Africa";
+        state.view = { rotate: [-20, 0, 0], scale: 400 };
+      } else if (payload.continent === "Asia") {
+        state.region = "Asia";
+        state.view = { rotate: [-90, -30, 0], scale: 450 };
+      } else if (payload.continent === "Europe") {
+        state.region = "Europe";
+        state.view = { rotate: [-10, -52, 0], scale: 1200 };
+      }
+    },
     AddLongLat(state, { payload }: PayloadAction<any>) {
       state.longlat = payload.longlat;
     },
