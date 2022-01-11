77e18d461759fd32073e6961e70b2ddfe4f92b2f
hansie95 <hansie.schmidt95@gmail.com>
2022-01-11 13:24:20 +0100

    feat(Covid-stat): Separate index.tsx components

---
+++ components/CountryData.tsx
@@ -0,0 +1,26 @@
+import { Flex, Box, Center } from "@chakra-ui/layout";
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { covidDataSelector } from "../store/selectors/selector";
+
+import MapChart from "./Map";
+import StatisticCard from "./StatisticCard";
+
+const CountryDatas = () => {
+  const data = useSelector(covidDataSelector);
+
+  if (data.results > 0) {
+    return (
+      <Flex wrap="wrap">
+        <Center mx="auto" w="710px" boxShadow="dark-lg" rounded="md" bg="white">
+          <MapChart />
+        </Center>
+        <StatisticCard country={data} />
+      </Flex>
+    );
+  }
+  return <Box></Box>;
+};
+
+export default CountryDatas;
