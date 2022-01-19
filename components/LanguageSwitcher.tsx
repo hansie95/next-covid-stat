745f03e4a98e94e0e8c4f3d4b0a09dda4dfceeb5
hansie95 <hansie.schmidt95@gmail.com>
2022-01-19 10:35:16 +0100

    feat(Covid-stat): Language switcher component created

---
+++ components/LanguageSwitcher.tsx
@@ -0,0 +1,60 @@
+import { Center, GridItem } from "@chakra-ui/layout";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { languageSelector } from "../store/selectors/languageSelector";
+import { languageSelectActions } from "../store/slices/languageSlice";
+
+const useLanguageSwitcher = () => {
+  const languageSelect = useSelector(languageSelector);
+  const dispatch = useDispatch();
+  const language =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("language")
+      : null;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      dispatch(languageSelectActions.langueageSwitcher(language));
+    }
+  });
+
+  const languageHunSwitcher = () => {
+    dispatch(languageSelectActions.langueageSwitcher("hun"));
+    localStorage.setItem("language", "hun");
+  };
+  const languageEngSwitcher = () => {
+    dispatch(languageSelectActions.langueageSwitcher("eng"));
+    localStorage.setItem("language", "eng");
+  };
+  return { languageSelect, languageHunSwitcher, languageEngSwitcher };
+};
+
+const LanguageSwitcher = () => {
+  const { languageSelect, languageHunSwitcher, languageEngSwitcher } =
+    useLanguageSwitcher();
+
+  return (
+    <GridItem colStart={1} alignItems="center" my="auto">
+      <Center>
+        <ButtonGroup size="sm" isAttached variant="outline">
+          <Button
+            isActive={languageSelect === "hun" && true}
+            onClick={languageHunSwitcher}
+          >
+            HUN
+          </Button>
+          <Button
+            isActive={languageSelect === "eng" && true}
+            onClick={languageEngSwitcher}
+          >
+            ENG
+          </Button>
+        </ButtonGroup>
+      </Center>
+    </GridItem>
+  );
+};
+
+export default LanguageSwitcher;
