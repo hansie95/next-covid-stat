20cfad5b6560374cadfbc266c67b9f30cdb7a033
hansie95 <hansie.schmidt95@gmail.com>
2022-01-19 10:36:24 +0100

    feat(Covid-stat): Components adjustment for language swiitcher

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,27 @@
 import { Box, Heading } from "@chakra-ui/layout";
+import { Grid } from "@chakra-ui/react";
 import React from "react";
+import { useSelector } from "react-redux";
 
+import { languageSelector } from "../store/selectors/languageSelector";
+import { texts } from "../languageTexts";
+
+import LanguageSwitcher from "./LanguageSwitcher";
 import SearchBar from "./SearchBar";
 import SpinPicture from "./SpinPicture";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
+  const language = useSelector(languageSelector);
   return (
     <Box textAlign="center" mb="50px" mt="45px">
-      <SpinPicture />
+      <Grid templateColumns="repeat(5, 1fr)">
+        <LanguageSwitcher />
+        <SpinPicture />
+        <ThemeSwitcher />
+      </Grid>
       <Heading mt="20px" size="xl">
-        Welcome Covid Stat v1
+        {language === "hun" ? texts.hun.welcome : texts.eng.welcome}
       </Heading>
       <Box mt="10px" mb="30px">
         <SearchBar />
